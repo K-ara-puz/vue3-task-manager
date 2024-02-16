61b222460282505a60161840a56d0a380823aeee
Viktoriya Okseniuk <mis.viktoriya@hotmail.com>
2024-02-16 14:39:01 +0200

    init task manager app

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav class="flex gap-5 font-bold border-b-2 border-gray-700 mb-5 pb-2">
+        <RouterLink to="/" class="text-green-700 hover:text-blue-400">Home</RouterLink>
+        <RouterLink to="/about" class="text-green-700 hover:text-blue-400">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
